0b50afc09411cdbe8629b1ce54a013c14226326b
ilovelili <route666@live.cn>
2019-08-05 12:10:22 +0900

    parse user info

--- jwt.go
+++ jwt.go
@@ -1,20 +1,15 @@
 package sharedlib
 
 import (
-	"errors"
-	"fmt"
+	"encoding/json"
 	"sync"
 
-	jwt "github.com/dgrijalva/jwt-go"
-	"github.com/kelvinji2009/graphql"
-	"github.com/lestrrat-go/jwx/jwk"
-
 	authing "github.com/Authing/authing-go-sdk"
-	prettyjson "github.com/hokaccha/go-prettyjson"
+	"github.com/kelvinji2009/graphql"
+	"fmt"
 )
 
 var (
-	keyset   *jwk.Set
 	once     sync.Once
 	instance *Client
 )
@@ -22,21 +17,16 @@ var (
 // Client authing client wrapper
 type Client struct {
 	AuthingClient *authing.Client
+	ClientID      string
 }
 
 // NewAuthClient  new authing client
 func NewAuthClient(clientID, appSecret string) *Client {
 	once.Do(func() {
 		client := authing.NewClient(clientID, appSecret, false)
-		// Enable debug info for graphql client, just comment it if you want to disable the debug info
-		client.Client.Log = func(s string) {
-			b := []byte(s)
-			pj, _ := prettyjson.Format(b)
-			fmt.Println(string(pj))
-		}
-
 		instance = &Client{
 			AuthingClient: client,
+			ClientID:      clientID,
 		}
 	})
 
@@ -57,39 +47,23 @@ func (c *Client) VerifyLogin(idtoken string) (bool, error) {
 	return bool(q.CheckLoginStatus.Status), nil
 }
 
-// ParseJWT parse JWT
-func ParseJWT(idtoken, jwks string) (claims jwt.MapClaims, token *jwt.Token, err error) {
-	// Get json web token key set https://auth0.com/docs/jwks
-	ks, err := fetchKeySet(jwks)
+// ParseUserInfo parse user info based on user id
+func (c *Client) ParseUserInfo(userID string) ([]byte, error) {
+	p := authing.UserQueryParameter{
+		ID:               graphql.String(userID),
+		RegisterInClient: graphql.String(c.ClientID),
+	}
+
+	q, err := c.AuthingClient.User(&p)
 	if err != nil {
-		return
+		return []byte{}, err
 	}
-
-	keyset = ks
-	claims = jwt.MapClaims{}
-	token, err = jwt.ParseWithClaims(idtoken, claims, getKey)
-	return
-}
-
-func getKey(token *jwt.Token) (interface{}, error) {
-	// validate the alg
-	if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
-		return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+	user := q.User
+	if user.Blocked {
+		return []byte{}, fmt.Errorf("This user is blocked")
 	}
-
-	keyID, ok := token.Header["kid"].(string)
-	if !ok {
-		return nil, errors.New("expecting JWT header to have string kid")
+	if user.IsDeleted {
+		return []byte{}, fmt.Errorf("This user has been deleted")
 	}
-
-	if key := keyset.LookupKeyID(keyID); len(key) == 1 {
-		return key[0].Materialize()
-	}
-
-	return nil, errors.New("unable to find key")
-}
-
-// fetchKeySet fetch key set
-func fetchKeySet(jwks string) (*jwk.Set, error) {
-	return jwk.FetchHTTP(jwks)
+	return json.Marshal(user)
 }
--- jwt_test.go
+++ jwt_test.go
@@ -1,18 +1,31 @@
 package sharedlib_test
 
 import (
+	"fmt"
 	"testing"
 
+	sharedlib "github.com/ilovelili/dongfeng-shared-lib"
 	"github.com/stretchr/testify/assert"
-	"github.com/ilovelili/dongfeng-shared-lib"
+)
+
+const (
+	clientid  = "xxx"
+	secretkey = "yyy"
 )
 
 func TestVerifyLogin(t *testing.T) {
-	appid := "xxx"
-	secretkey := "yyy"
 	idtoken := "zzz"
-	client := sharedlib.NewAuthClient(appid, secretkey)
+	client := sharedlib.NewAuthClient(clientid, secretkey)
 	status, err := client.VerifyLogin(idtoken)
 	assert.True(t, status)
+	assert.Nil(t, err)
+}
+
+func TestParseUserInfo(t *testing.T) {
+	userid := "5d3d973d75115d234ffa762b"
+	client := sharedlib.NewAuthClient(clientid, secretkey)
+	userinfo, err := client.ParseUserInfo(userid)
+
+	fmt.Printf("user info is %s\n", userinfo)
 	assert.Nil(t, err)
 }
